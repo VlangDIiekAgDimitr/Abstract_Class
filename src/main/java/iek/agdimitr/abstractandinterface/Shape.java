89b0c010e6c90a91659846e3e2212ff0fae918a3
avlonitiss <avlonitiss@yahoo.co.uk>
2021-04-25 23:57:43 +0300

    abstract Shape class

--- src/main/java/iek/agdimitr/abstractandinterface/Shape.java
+++ src/main/java/iek/agdimitr/abstractandinterface/Shape.java
@@ -10,10 +10,28 @@ package iek.agdimitr.abstractandinterface;
  * @author avlonitiss
  */
 public abstract class Shape {
-     // declare fields
+     // Δήλωση πεδίων
     String objectName = " ";
  
-    // declare constructor
-    Shape(String name) { this.objectName = name; }
+    // δήλωση constructor
+    Shape(String name) { 
+        this.objectName = name; 
+    }
+    
+    // δήλωση non-abstract methods
+    // it has default implementation
+    public void moveTo(int x, int y)
+    {
+        System.out.println(this.objectName + " "
+                           + "has been moved to"
+                           + " x = " + x + " and y = " + y);
+    }
+    
+    // δήλωση abstract methods που
+    // υλοποιούνται στις υποκλάσεις
+    // ή σε μία υποκλάση
+    
+    abstract public double area();
+    abstract public void draw();
     
 }
