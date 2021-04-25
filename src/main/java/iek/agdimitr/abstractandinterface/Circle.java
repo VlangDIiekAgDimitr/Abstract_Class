b4ad0c9d918dee2410453508680c7a9c8871c3be
avlonitiss <avlonitiss@yahoo.co.uk>
2021-04-26 00:39:27 +0300

    Circle extends Shape class

---
+++ src/main/java/iek/agdimitr/abstractandinterface/Circle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iek.agdimitr.abstractandinterface;
+
+/**
+ *
+ * @author User
+ */
+public class Circle extends Shape {
+
+    double pi = 3.14;
+    int radius;
+ 
+    // constructor
+    Circle(int radius, String name)
+    {
+        super(name);
+        this.radius = radius;
+    }
+ 
+    @Override
+    public double area() {
+        return (double)((pi * radius * radius) / 2);
+    }
+
+    @Override
+    public void draw() {
+    System.out.println("Circle has been drawn ");
+    }
+    
+}
