1a6f067b226de6b5b87b7afdcd93ecc626a09d33
avlonitiss <avlonitiss@yahoo.co.uk>
2021-04-25 23:54:27 +0300

    abstract Shape class

---
+++ src/main/java/iek/agdimitr/abstractandinterface/Shape.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iek.agdimitr.abstractandinterface;
+
+/**
+ *
+ * @author avlonitiss
+ */
+public abstract class Shape {
+     // declare fields
+    String objectName = " ";
+ 
+    // declare constructor
+    Shape(String name) { this.objectName = name; }
+    
+}
